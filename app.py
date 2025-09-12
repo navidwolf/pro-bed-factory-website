9a83d46665308f730f3952ad143c36609c5bb500
navidwolf <navidwolf@gmail.com>
2025-09-12 17:24:55 +0330

    Add Procfile and update app.py for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, Response
 from datetime import datetime
+import os
 
 
 app = Flask(__name__)
@@ -60,5 +61,6 @@ response = Response('\n'.join(xml), mimetype='application/xml')
 return response
 
 
-if __name__ == '__main__':
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
